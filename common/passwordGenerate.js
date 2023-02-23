568c477775b37c86ffe07767a692b71822dd9400
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-02-23 08:57:28 +0545

    Hashing

--- common/passwordGenerate.js
+++ common/passwordGenerate.js
@@ -1,11 +1,27 @@
 ((passwordGenerate)=>{
-    passwordGenerate.auto=()=>{
+    passwordGenerate.auto=async()=>{
+
+        const fs = require('fs'); 
+
+        const hash=require('./hasher')
             var length = 8,
                 charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
                 retVal = "";
             for (var i = 0, n = charset.length; i < length; ++i) {
                 retVal += charset.charAt(Math.floor(Math.random() * n));
             }
-            return(retVal);
+            // console.log(retVal);
+            fs.appendFile('./../Text.txt',retVal, 'utf8',
+    // callback function
+    function(err) {     
+        if (err) throw err;
+        // if no error
+        console.log("Data is appended to file successfully.")
+});
+            const salt=await hash.createSalt();
+            // console.log("=======",retVal,salt);
+            const pw=await hash.computeHash(retVal,salt);
+            console.log(pw);
+            return pw
     }
 })(module.exports)
--- routes/user/methods/login.js
+++ routes/user/methods/login.js
@@ -2,17 +2,22 @@
 (()=>{
     const schedule=require('node-schedule')
     const mysqlHelper=require('./../../../common/index')
-    module.exports=(req,res,next)=>{
-        // const {email,password}=req.body;
-        const job = schedule.scheduleJob('* * * * * *', function(){
-            console.log('Today is recognized by Rebecca Black!');
-          });
-        console.log(job.scheduleJob);
-        // const sql="Select name from user where email='"+email+"' and password='"+password+"'";
-        // const response=await mysqlHelper.mysqlHelp.query(sql);
-        // console.log(response[0]);
-        // if(response[0]){
-        //     res.send("www.facebook.com");
-        // }
+    const hash=require('./../../../common/hasher')
+    module.exports=async(req,res,next)=>{
+        const {email,password}=req.body;
+        // const job = schedule.scheduleJob('* * * * * *', function(){
+        //     console.log('Today is recognized by Rebecca Black!');
+
+        //   });
+        // console.log(job.scheduleJob);
+        // if(hash.comparePassword);
+        const sql="Select password from user where email='"+email+"'";
+        const response=await mysqlHelper.mysqlHelp.query(sql);
+        console.log("login =======",password,response[0][0].password);
+        if(hash.comparePassword(password,response[0][0].password)){
+            if(response[0]){
+                res.send("Password match");
+            }
+        }
     }
 })()
